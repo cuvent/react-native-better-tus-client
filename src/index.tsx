0fdc4ef83ce5686fb4538ee192fdc6111703e17b
Hanno Gödecke <hgoedecke@cuvent.com>
2021-02-24 11:48:34 +0100

    feat: updated types

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,37 @@
 import { NativeModules } from 'react-native';
 
+export type BetterTusClientEventNames =
+  | 'onGlobalProgress'
+  | 'onProgress'
+  | 'onSuccess'
+  | 'onFailure';
+
+type OnProgressEvent = {
+  bytesUploaded: number;
+  bytesRemaining: number;
+};
+
+export type BetterTusClientEventOnGlobalProgress = OnProgressEvent;
+export type BetterTusClientEventOnProgress = OnProgressEvent & {
+  uploadId: string;
+};
+export type BetterTusClientEventOnSuccess = {
+  uploadId: string;
+  url: string;
+};
+export type BetterTusClientEventOnFailure = {
+  uploadId?: string;
+  error?: string;
+  message?: string;
+};
+
 type BetterTusClientType = {
-  multiply(a: number, b: number): Promise<number>;
+  createUpload(
+    withId: string,
+    filePath: string,
+    fileType: string,
+    headers?: Record<string, string>
+  ): Promise<void>;
 };
 
 const { BetterTusClient } = NativeModules;
