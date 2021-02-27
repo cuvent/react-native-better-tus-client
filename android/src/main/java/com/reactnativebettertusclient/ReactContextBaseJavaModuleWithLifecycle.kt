4e2f8e8df4fa0cff3d50dd4315796e9deaa1b610
Hanno Gödecke <hgoedecke@cuvent.com>
2021-02-27 12:33:02 +0100

    docs: added description what this class does

--- android/src/main/java/com/reactnativebettertusclient/ReactContextBaseJavaModuleWithLifecycle.kt
+++ android/src/main/java/com/reactnativebettertusclient/ReactContextBaseJavaModuleWithLifecycle.kt
@@ -7,6 +7,10 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 
+/**
+ * This class gets the lifecycle from the applications activity and provide the class
+ * to be a LifecycleOwner.
+ */
 abstract class ReactContextBaseJavaModuleWithLifecycle(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext), LifecycleOwner {
   private val activityLifecycle: Lifecycle by lazy {
     ((reactContext as ReactContext).currentActivity as AppCompatActivity).lifecycle
